c15d3a970da90176be7090b454b47fad83312819
Jian <94353883+JianRen302708248@users.noreply.github.com>
2023-01-31 12:03:07 +0100

    methodes opdrachten afgemaakt

--- classes/Person.php
+++ classes/Person.php
@@ -24,7 +24,44 @@ class Person
         echo "The property name of this object is: $name";
     }
 
+    // Set - methode
     public function setGender(string $gender) {
         $this->gender = $gender;
     }
+    public function setName(string $name) {
+        $this->name = $name;
+    }
+    public function setAge(int $age) {
+        $this->age = $age;
+    }
+    public function setIsStudent(bool $isStudent) {
+        $this->isStudent = $isStudent;
+    }
+    public function setAverageGrade(int $averageGrade) {
+        $this->averageGrade = $averageGrade;
+    }
+
+    //Get - methode
+    public function getGender() {
+        return $this->gender;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function getAge() {
+        return $this->age;
+    }
+    public function getIsStudent() {
+        return $this->isStudent;
+    }
+    public function getAverageGrade() {
+        return $this->averageGrade;
+    }
+
+    public function getGegevens() {
+        echo ('De gegevens van Henk zijn: <br>');
+        echo ('Leeftijd: ' . $this->age .'<br>');
+        echo ('Geslacht: ' . $this->gender . '<br>');
+    }
+
 }
